724794218a54b61c8081bd4ffd36caf30c4f3663
Home <ildar86@live.ru>
2022-11-10 13:37:36 +0500

    ДЗ-2 выполнено Задание 3.

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,11 @@ public class Main {
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i); }
 
+        //Задание 3.
+        System.out.println("Циклы ч.1. ДЗ-2. Задача 3");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i); }
+
 
 
 
