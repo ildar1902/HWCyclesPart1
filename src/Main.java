d81fad902d2b63a9dc3a99e541507cfcd0fb666b
Home <ildar86@live.ru>
2022-11-10 13:11:58 +0500

    Выполнены первые 3 задачи задания 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание 1.
+        System.out.println("Циклы ч.1. ДЗ. Задача 1.");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i); }
+
+        //Задание 2.
+        System.out.println("Циклы ч.1. ДЗ. Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i); }
+
+        //Задание 3.
+        System.out.println("Циклы ч.1. ДЗ. Задача 3");
+        for (int i = 0; i <= 17 ; i++) {
+            System.out.println(i++); }
+
+
+    }
+}
