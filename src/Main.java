11fe92cba5c3da858c12181f1dc65acd02545dad
Home <ildar86@live.ru>
2022-11-10 13:31:54 +0500

    ДЗ-2 выполнено Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 public class Main {
     public static void main(String[] args) {
 
+        //ДЗ-1.
+
         //Задание 1.
-        System.out.println("Циклы ч.1. ДЗ. Задача 1.");
+        System.out.println("Циклы ч.1. ДЗ-1. Задача 1.");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i); }
 
         //Задание 2.
-        System.out.println("Циклы ч.1. ДЗ. Задача 2");
+        System.out.println("Циклы ч.1. ДЗ-1. Задача 2");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i); }
 
         //Задание 3.
-        System.out.println("Циклы ч.1. ДЗ. Задача 3");
+        System.out.println("Циклы ч.1. ДЗ-1. Задача 3");
         for (int i = 0; i <= 17; i = i + 2) {
             System.out.println(i); }
 
         //Задание 4.
-        System.out.println("Циклы ч.1. ДЗ. Задача 4");
+        System.out.println("Циклы ч.1. ДЗ-1. Задача 4");
         for (int i = 10; i >= -10; i-- ) {
             System.out.println(i); }
+
+        //ДЗ-2.
+
+        //Задание 1.
+        System.out.println("Циклы ч.1. ДЗ-2. Задача 1");
+        for (int i = 1904; i <= 2096; i = i +4) {
+            System.out.println(i + " год является високосным!"); }
+
 
 
     }
